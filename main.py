36b8ee74b5f7f076372ad583f4fb098376a0d068
Lizzie Williams <lsw120@ic.ac.uk>
2023-01-03 16:11:33 +0000

    Adding graphs and utility function

--- main.py
+++ main.py
@@ -1,16 +1,41 @@
 from regretmatching.blotto import BLOTTOPlayer
 import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.lines as mlines
 
+blotto_strategies = [(5,0,0), (4,0,1), (4,1,0), (3,2,0), (3,0,2), (3,1,1), (2,3,0), (2,0,3), (2,2,1), 
+(2,1,2), (1,4,0), (1,0,4), (1,1,3), (1,3,1), (1,2,2), (0,5,0), (0,4,1), (0,3,2), (0,2,3), (0,1,4), (0,0,5)]
 a = BLOTTOPlayer()
 b = BLOTTOPlayer()
-t = 10000
+t = 3000001
+alist = []
+blist = []
+ylist = []
 for i in range(0, t):
     a_move = a.move()
     b_move = b.move()
     a.learn_from(b_move)
     b.learn_from(a_move)
+    alist.append(a.current_best_response())
+    blist.append(b.current_best_response())
+    ylist.append(i)
+   
 
-_2e = np.round(2 * np.max([a.eps(), b.eps()]), 3)
+
+_2e = np.round(2 * np.max([a.eps(), b.eps()]), 10)
 a_ne = a.current_best_response()
 b_ne = b.current_best_response()
+plt.plot(ylist, alist, color='blue', alpha=0.3)
+plt.plot(ylist, blist, color='red', alpha=0.3)
+plt.title('Graph showing how the propabilities of the strategies of Colonel Blotto and Boba Fett converge')
+plt.ylabel('Probability strategy is optimal')
+plt.xlabel('Time (Number of Iterations)')
+blue_line = mlines.Line2D([], [], color='blue',
+                          markersize=15, label='Colonel Blotto')
+red_line = mlines.Line2D([], [], color='red',
+                          markersize=15, label='Boba Fett')
+plt.legend(handles=[blue_line, red_line])
+plt.show()
 print("{0} - nash equilibrium for BLOTTO: {1}, {2}".format(_2e, a_ne, b_ne))
+u1= a.utility()
+print(str(u1))
--- regretmatching/blotto.py
+++ regretmatching/blotto.py
@@ -1,27 +1,27 @@
 from regretmatching.model import RegretMatchingDecisionMaker, Expert
 import numpy as np
 
-FEZZ = Expert('5,0,0')
-FRZO = Expert('4,0,1')
-FROZ = Expert('4,1,0')
-THTWZ = Expert('3,2,0')
-THZTW = Expert('3,0,2')
-THOO = Expert('3,1,1')
-TWTHZ = Expert('2,3,0')
-TWZTH = Expert('2,0,3')
-TWTWO = Expert('2,2,1')
-TWOTW = Expert('2,1,2')
-OFOZ = Expert('1,4,0')
-OZFO = Expert('1,0,4')
-OOTH = Expert('1,1,3')
-OTHO = Expert('1,3,1')
-OTWTW = Expert('1,2,2')
-ZFEZ = Expert('0,5,0')
-ZFOO = Expert('0,4,1')
-ZTHTW = Expert('0,3,2')
-ZTWTH = Expert('0,2,3')
-ZOFO = Expert('0,1,4')
-ZZFE = Expert('0,0,5')
+FEZZ = Expert([5,0,0])
+FRZO = Expert([4,0,1])
+FROZ = Expert([4,1,0])
+THTWZ = Expert([3,2,0])
+THZTW = Expert([3,0,2])
+THOO = Expert([3,1,1])
+TWTHZ = Expert([2,3,0])
+TWZTH = Expert([2,0,3])
+TWTWO = Expert([2,2,1])
+TWOTW = Expert([2,1,2])
+OFOZ = Expert([1,4,0])
+OZFO = Expert([1,0,4])
+OOTH = Expert([1,1,3])
+OTHO = Expert([1,3,1])
+OTWTW = Expert([1,2,2])
+ZFEZ = Expert([0,5,0])
+ZFOO = Expert([0,4,1])
+ZTHTW = Expert([0,3,2])
+ZTWTH = Expert([0,2,3])
+ZOFO = Expert([0,1,4])
+ZZFE = Expert([0,0,5])
 
 BLOTTO_EXPERTS = [FEZZ, FRZO, FROZ, THTWZ, THZTW, THOO, TWTHZ, TWZTH, TWTWO, 
 TWOTW, OFOZ, OZFO, OOTH, OTHO, OTWTW, ZFEZ, ZFOO, ZTHTW, ZTWTH, ZOFO, ZZFE]
@@ -72,3 +72,17 @@ class BLOTTOPlayer(RegretMatchingDecisionMaker):
 
     def eps(self):
         return np.max(self.regrets / self.games_played)
+    
+    def utility(self):
+        utility1 = 0
+        for i in range(len(BLOTTO_EXPERTS)):
+            for j in range(len(BLOTTO_EXPERTS)):
+                sum = 0
+                for k in range(3):
+                    if BLOTTO_EXPERTS[i].id[k] > BLOTTO_EXPERTS[j].id[k]:
+                        sum += 1
+                utility1 += sum
+        return utility1
+
+
+
